72ec1f27cce7cd4c68e88bda545a5a52e444f64a
Fabiano <fabianoyoschitaki@gmail.com>
2017-06-14 00:59:06 -0300

    png

--- fabiano_tutoriais/imbalanced_learn/over-sampling/plot_random_over_sampling.py
+++ fabiano_tutoriais/imbalanced_learn/over-sampling/plot_random_over_sampling.py
@@ -23,10 +23,17 @@ from imblearn.over_sampling import RandomOverSampler
 print(__doc__)
 
 # Generate the dataset
-X, y = make_classification(n_classes=2, class_sep=2, weights=[0.1, 0.9],
-                           n_informative=3, n_redundant=1, flip_y=0,
-                           n_features=20, n_clusters_per_class=1,
-                           n_samples=200, random_state=10)
+X, y = make_classification(
+	n_classes=2, 
+	class_sep=2, 
+	weights=[0.1, 0.9],
+    n_informative=3, 
+	n_redundant=1, 
+	flip_y=0,
+	n_features=20, 
+	n_clusters_per_class=1,
+	n_samples=200, 
+	random_state=10)
 
 # Instanciate a PCA object for the sake of easy visualisation
 pca = PCA(n_components=2)
