e01396e59ab4149fb9d054833434abdb75213e01
Fabiano <fabianoyoschitaki@gmail.com>
2017-07-09 01:19:33 -0300

    regression quiz

---
+++ regression/studentRegression.py
@@ -0,0 +1,21 @@
+def studentReg(ages_train, net_worths_train):
+	### import the sklearn regression module, create, and train your regression
+    from sklearn import linear_model
+    
+	### name your regression reg
+    reg = linear_model.LinearRegression()
+		
+    ### your code goes here!
+    reg.fit(ages_train, net_worths_train)   
+	
+	print "\nfabiano prediction:", reg.predict([[29]]) 
+	print "\nslope:", reg.coef_
+	print "\nintercept:", reg.intercept_
+	
+	print "\n#### stats on test dataset ####"
+	print "\nr-squared score:", reg.score(ages_test, net_worths_test)
+    
+	print "\n#### stats on training dataset ####"
+	print "\nr-squared score:", reg.score(ages_train, net_worths_train)
+    
+    return reg
