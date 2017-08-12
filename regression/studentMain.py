489fa22c218989c43b93df747756a1af09b3bdf5
Fabiano <fabianoyoschitaki@gmail.com>
2017-08-11 23:06:15 -0300

    regression...

--- regression/studentMain.py
+++ regression/studentMain.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-
 import numpy
 import matplotlib
 matplotlib.use('agg')
@@ -7,16 +6,11 @@ matplotlib.use('agg')
 import matplotlib.pyplot as plt
 from studentRegression import studentReg
 from class_vis import prettyPicture, output_image
-
 from ages_net_worths import ageNetWorthData
 
 ages_train, ages_test, net_worths_train, net_worths_test = ageNetWorthData()
 
-
-
-reg = studentReg(ages_train, net_worths_train)
-
-
+reg = studentReg(ages_train, net_worths_train, ages_test, net_worths_test)
 plt.clf()
 plt.scatter(ages_train, net_worths_train, color="b", label="train data")
 plt.scatter(ages_test, net_worths_test, color="r", label="test data")
@@ -24,7 +18,5 @@ plt.plot(ages_test, reg.predict(ages_test), color="black")
 plt.legend(loc=2)
 plt.xlabel("ages")
 plt.ylabel("net worths")
-
-
 plt.savefig("test.png")
 output_image("test.png", "png", open("test.png", "rb").read())
