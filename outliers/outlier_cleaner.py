fb2b94370e63593374acc4fc664f4ff46d8205ee
Fabiano <fabianoyoschitaki@gmail.com>
2017-08-20 18:43:50 -0300

    refit

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -2,19 +2,39 @@
 
 
 def outlierCleaner(predictions, ages, net_worths):
-    """
-        Clean away the 10% of points that have the largest
-        residual errors (difference between the prediction
-        and the actual net worth).
+	"""
+		Clean away the 10% of points that have the largest
+		residual errors (difference between the prediction
+		and the actual net worth).
 
-        Return a list of tuples named cleaned_data where 
-        each tuple is of the form (age, net_worth, error).
-    """
-    
-    cleaned_data = []
+		Return a list of tuples named cleaned_data where 
+		each tuple is of the form (age, net_worth, error).
+	"""
+	
+	cleaned_data = []
 
-    ### your code goes here
-
-    
-    return cleaned_data
+	### your code goes here
+	
+	error = list((net_worths - predictions)**2) #squared error	
+	#print "error variable is:" + str(type(error))
+	#cont = 0
+	#for err in error:
+	#	print str(err) + " = " + str(net_worths[cont]) + "-" + str(predictions[cont])
+	#	cont += 1
+	
+	#concatenate arrays
+	cleaned_data = zip(ages, net_worths, error)
+	#print "cleaned_data: " + str(type(cleaned_data)) + str(cleaned_data[0])
+	#for item in cleaned_data:
+	#	print str(item)
+	
+	#sort data by error ascending
+	cleaned_data = sorted(cleaned_data, key = lambda tup: tup[2])
+	#print "cleaned_data sorted by tup[2]: " + str(type(cleaned_data)) + str(cleaned_data[0])
+	#for item in cleaned_data:
+	#	print str(item)
+		
+	#keep first 80 records
+	cleaned_data = cleaned_data[:80]
+	return cleaned_data
 
--- outliers/outlier_removal_regression.py
+++ outliers/outlier_removal_regression.py
@@ -22,13 +22,14 @@ ages_train, ages_test, net_worths_train, net_worths_test = train_test_split(ages
 
 ### fill in a regression here!  Name the regression object reg so that
 ### the plotting code below works, and you can see what your regression looks like
+from sklearn.linear_model import LinearRegression
 reg = LinearRegression()
-reg.fit(feature_train, target_train)
+reg.fit(ages_train, net_worths_train)
+print "==== Before refitting data ===="
 print "Coeff: ", reg.coef_
 print "Intercept: ", reg.intercept_
-print "Score: ", reg.score(feature_train, target_train)
-print "Score: ", reg.score(feature_test, target_test)
-
+print "Score Train: ", reg.score(ages_train, net_worths_train)
+print "Score Test: ", reg.score(ages_test, net_worths_test)
 
 try:
     plt.plot(ages, reg.predict(ages), color="blue")
@@ -36,7 +37,6 @@ except NameError:
     pass
 plt.scatter(ages, net_worths)
 plt.show()
-
 
 ### identify and remove the most outlier-y points
 cleaned_data = []
@@ -47,30 +47,29 @@ except NameError:
     print "your regression object doesn't exist, or isn't name reg"
     print "can't make predictions to use in identifying outliers"
 
-
-
-
-
-
-
 ### only run this code if cleaned_data is returning data
 if len(cleaned_data) > 0:
-    ages, net_worths, errors = zip(*cleaned_data)
-    ages       = numpy.reshape( numpy.array(ages), (len(ages), 1))
-    net_worths = numpy.reshape( numpy.array(net_worths), (len(net_worths), 1))
+	ages, net_worths, errors = zip(*cleaned_data)
+	ages       = numpy.reshape( numpy.array(ages), (len(ages), 1))
+	net_worths = numpy.reshape( numpy.array(net_worths), (len(net_worths), 1))
 
-    ### refit your cleaned data!
-    try:
-        reg.fit(ages, net_worths)
-        plt.plot(ages, reg.predict(ages), color="blue")
-    except NameError:
-        print "you don't seem to have regression imported/created,"
-        print "   or else your regression object isn't named reg"
-        print "   either way, only draw the scatter plot of the cleaned data"
-    plt.scatter(ages, net_worths)
-    plt.xlabel("ages")
-    plt.ylabel("net worths")
-    plt.show()
+	### refit your cleaned data!
+	try:
+		reg.fit(ages, net_worths)
+		print "==== After refitting data ===="
+		print "Coeff: ", reg.coef_
+		print "Intercept: ", reg.intercept_
+		print "Score Train: ", reg.score(ages_train, net_worths_train)
+		print "Score Test: ", reg.score(ages_test, net_worths_test)
+		plt.plot(ages, reg.predict(ages), color="blue")
+	except NameError:
+		print "you don't seem to have regression imported/created,"
+		print "   or else your regression object isn't named reg"
+		print "   either way, only draw the scatter plot of the cleaned data"
+	plt.scatter(ages, net_worths)
+	plt.xlabel("ages")
+	plt.ylabel("net worths")
+	plt.show()
 
 
 else:
